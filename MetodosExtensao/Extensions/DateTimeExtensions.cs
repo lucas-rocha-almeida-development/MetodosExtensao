20f2c5045fc28a651fc5887982f108dd13254811
Lucas Almeida <lucalmr12@gmail.com>
2023-08-23 20:27:51 -0300

    Adicionar arquivos de projeto.

---
+++ MetodosExtensao/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodosExtensao.Extensions
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+
+            TimeSpan duracao = DateTime.Now.Subtract(thisObj);
+
+            if(duracao.TotalDays < 24.0)
+            {
+                return duracao.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " Horas ";
+
+            }
+            else
+            {
+                return duracao.TotalDays.ToString("F1",CultureInfo.InvariantCulture) + " Dias ";
+            }
+        }
+    }
+}
---
+++ MetodosExtensao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetodosExtensao.Extensions;
+
+namespace MetodosExtensao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2023,07,21,8,10,45);
+            Console.WriteLine(dt.ElapsedTime());
+            // criando string utilizando o metodo Cut / corte
+            string s1 = "Bom dia queridos estudantes!";
+            Console.WriteLine(s1.Cut(10));
+
+        }
+    }
+}
